a49aaec5d06b1d5b90e6600bf6c56e65de0db74a
labkomers <labkom@mipa.uns.ac.id>
2021-05-18 11:34:43 +0700

    Memperbaiki tahun angkatan

--- resources/views/User/PeminjamanAlat/create.blade.php
+++ resources/views/User/PeminjamanAlat/create.blade.php
@@ -49,15 +49,15 @@
                         <label class="form-label" for="angkatan">Angkatan</label>
                         <select id="angkatan" name="angkatan" class="form-select @error('angkatan') error @enderror">
                             <option value=""></option>
-                            <option value="2016">2014</option>
-                            <option value="2016">2015</option>
+                            <option value="2014">2014</option>
+                            <option value="2015">2015</option>
                             <option value="2016">2016</option>
                             <option value="2017">2017</option>
                             <option value="2018">2018</option>
                             <option value="2019">2019</option>
                             <option value="2020">2020</option>
                             <option value="2021">2021</option>
-                            <option value="2021">2022</option>
+                            <option value="2022">2022</option>
                         </select>
                         @error('angkatan')
                             <div class="form-error">{{ $message }}</div>
